87c6fdccea0bddf4c2c7458ae0806ada79529bcc
diggerdu <diggerdu@hotmail.com>
2016-06-13 21:25:44 +0800

    sometings

---
+++ DisplayImage.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <cmath>
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+const int BLUR_BLOCK_SIZE = 5;
+using namespace cv;
+/* Return if absolute value of X is within the range of [MIN, MAX]. */
+bool inRange(double x, double min, double max)
+{
+	x = fabs(x);
+	return (min <= x) && (x <= max);
+}
+
+Mat pretreat(Mat original)
+{
+	Mat treated;
+	cvtColor(original, treated, CV_BGR2GRAY);
+	GaussianBlur(treated, treated, Size(BLUR_BLOCK_SIZE, BLUR_BLOCK_SIZE), 0);
+	equalizeHist(treated, treated);
+	///cautions
+	threshold(treated, treated, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+	return treated;		
+}
+
+double distance(Point pt1, Point pt2)
+{
+	return sqrt(sqrt(pt1.x - pt2.x) + sqrt(pt1.y - pt2.y));
+}
+
+int main(int argc, char** argv )
+{
+    if ( argc != 2 )
+    {
+        printf("usage: DisplayImage.out <Image_Path>\n");
+        return -1;
+    }
+
+    Mat image;
+    image = imread( argv[1], 1 );
+
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    //namedWindow("Display Image", WINDOW_AUTOSIZE );
+    //imshow("Display Image", image);
+	Mat out;
+	out = pretreat(image); 
+	namedWindow("display", WINDOW_AUTOSIZE);
+	imshow("Display Image", out);
+    waitKey(0);
+
+    return 0;
+}
